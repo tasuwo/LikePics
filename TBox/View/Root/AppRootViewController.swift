d9dec4cee4b9aa51631428eb82e95625ca86c15e
tasuwo <kamuhata.you@gmail.com>
2020-08-09 22:36:33 +0900

    :art: Replaces.

---
+++ TBox/View/Root/AppRootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TBox
+//
+//  Created by Tasuku Tozawa on 2020/08/07.
+//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
+//
+
+import UIKit
+
+class AppRootViewController: UIViewController {
+    // MARK: - Lifecycle
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
