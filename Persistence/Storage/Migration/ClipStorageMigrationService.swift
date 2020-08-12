64c9ea4b8cb9720a14ed68eadf5215f798d0f57a
tasuwo <kamuhata.you@gmail.com>
2020-08-13 02:28:54 +0900

    :rocket: Make migration fast.

--- Persistence/Storage/Migration/ClipStorageMigrationService.swift
+++ Persistence/Storage/Migration/ClipStorageMigrationService.swift
@@ -34,33 +34,14 @@ enum ClipStorageMigrationService {
                 clippedLargeImage.key = clippedLargeImage.makeKey()
                 migration.create(ClippedImageObject.className(), value: clippedLargeImage)
 
-                // ClippedImage for thumbnail
-
-                let thumbnailClippedImage = ClippedImageObject()
-
-                thumbnailClippedImage.clipUrl = clipUrl
-
-                let thumbnailImageUrlString = Self.resolveThumbnailImageUrlString(fromUrlString: largeImageUrlString)
-                thumbnailClippedImage.imageUrl = thumbnailImageUrlString
-
-                let thumbnailImageData = try! Data(contentsOf: URL(string: thumbnailImageUrlString)!)
-                thumbnailClippedImage.image = UIImage(data: thumbnailImageData)!.pngData()!
-
-                thumbnailClippedImage.key = thumbnailClippedImage.makeKey()
-
-                migration.create(ClippedImageObject.className(), value: thumbnailClippedImage)
-
-                // ClipItem
-
-                let thumbnailImageSize = Self.calcImageSize(ofUrl: URL(string: thumbnailImageUrlString)!)
-                let largeImageSize = Self.calcImageSize(ofUrl: URL(string: largeImageUrlString)!)
+                let largeImageSize = UIImage(data: largeImageData)!.size
 
                 let clipItem = ClipItemObject()
                 clipItem.clipUrl = clipUrl
                 clipItem.clipIndex = index
-                clipItem.thumbnailImageUrl = thumbnailImageUrlString
-                clipItem.thumbnailHeight = Double(thumbnailImageSize.height)
-                clipItem.thumbnailWidth = Double(thumbnailImageSize.width)
+                clipItem.thumbnailImageUrl = largeImageUrlString
+                clipItem.thumbnailHeight = Double(largeImageSize.height)
+                clipItem.thumbnailWidth = Double(largeImageSize.width)
                 clipItem.largeImageUrl = largeImageUrlString
                 clipItem.largeImageHeight = Double(largeImageSize.height)
                 clipItem.largeImageWidth = Double(largeImageSize.width)
@@ -72,34 +53,6 @@ enum ClipStorageMigrationService {
             }
         }
         migration.deleteData(forType: "WebImageObject")
-    }
-
-    private static func resolveThumbnailImageUrlString(fromUrlString url: String) -> String {
-        guard url.contains("twimg") else { return url }
-        guard var components = URLComponents(string: url), let queryItems = components.queryItems else {
-            return url
-        }
-
-        let newQueryItems: [URLQueryItem] = queryItems
-            .compactMap { queryItem in
-                guard queryItem.name == "name" else { return queryItem }
-                return URLQueryItem(name: "name", value: "small")
-            }
-
-        components.queryItems = newQueryItems
-
-        return components.url?.absoluteString ?? url
-    }
-
-    private static func calcImageSize(ofUrl url: URL) -> CGSize {
-        if let imageSource = CGImageSourceCreateWithURL(url as CFURL, nil) {
-            if let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary? {
-                let pixelWidth = imageProperties[kCGImagePropertyPixelWidth] as! CGFloat
-                let pixelHeight = imageProperties[kCGImagePropertyPixelHeight] as! CGFloat
-                return .init(width: pixelWidth, height: pixelHeight)
-            }
-        }
-        return .zero
     }
 }
 
