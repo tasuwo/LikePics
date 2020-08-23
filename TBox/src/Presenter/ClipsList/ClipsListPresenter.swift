312853e18b376bb16c44d1cf7b373c72ba87e952
tasuwo <kamuhata.you@gmail.com>
2020-08-23 21:41:13 +0900

    :hammer: Add base class.

---
+++ TBox/src/Presenter/ClipsList/ClipsListPresenter.swift
@@ -0,0 +1,75 @@
+//
+//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
+//
+
+import Domain
+
+protocol ClipsListViewProtocol: AnyObject {
+    func startLoading()
+    func endLoading()
+    func showErrorMassage(_ message: String)
+    func reload()
+}
+
+class ClipsListPresenter {
+    enum ThumbnailLayer {
+        case primary
+        case secondary
+        case tertiary
+    }
+
+    weak var view: ClipsListViewProtocol?
+
+    private let storage: ClipStorageProtocol
+
+    private(set) var clips: [Clip] = []
+
+    // MARK: - Lifecycle
+
+    public init(storage: ClipStorageProtocol) {
+        self.storage = storage
+    }
+
+    // MARK: - Methods
+
+    func reload() {
+        guard let view = self.view else { return }
+
+        view.startLoading()
+        switch self.storage.readAllClips() {
+        case let .success(ClipsList):
+            self.clips = ClipsList.sorted(by: { $0.registeredDate > $1.registeredDate })
+            view.reload()
+        case let .failure(error):
+            view.showErrorMassage(Self.resolveErrorMessage(error))
+        }
+        view.endLoading()
+    }
+
+    func getImageData(for layer: ThumbnailLayer, in clip: Clip) -> Data? {
+        let nullableClipItem: ClipItem? = {
+            switch layer {
+            case .primary:
+                return clip.primaryItem
+            case .secondary:
+                return clip.secondaryItem
+            case .tertiary:
+                return clip.tertiaryItem
+            }
+        }()
+        guard let clipItem = nullableClipItem else { return nil }
+
+        switch self.storage.getImageData(ofUrl: clipItem.thumbnail.url, forClipUrl: clip.url) {
+        case let .success(data):
+            return data
+        case let .failure(error):
+            self.view?.showErrorMassage(Self.resolveErrorMessage(error))
+            return nil
+        }
+    }
+
+    private static func resolveErrorMessage(_ error: ClipStorageError) -> String {
+        // TODO: Error Handling
+        return "問題が発生しました"
+    }
+}
