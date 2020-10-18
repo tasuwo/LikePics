b96d449bed9443eb55d1829151067830bc850c97
tasuwo <kamuhata.you@gmail.com>
2020-10-18 21:02:08 +0900

    :sparkles: CGSizeに変換できる

--- Domain/ValueObject/ImageSize.swift
+++ Domain/ValueObject/ImageSize.swift
@@ -2,12 +2,19 @@
 //  Copyright © 2020 Tasuku Tozawa. All rights reserved.
 //
 
+import CoreGraphics
+
 // sourcery: AutoDefaultValue
 public struct ImageSize: Equatable {
     public static let zero = ImageSize(height: 0, width: 0)
 
     public let height: Double
     public let width: Double
+
+    public var cgSize: CGSize {
+        return CGSize(width: self.width,
+                      height: self.height)
+    }
 
     // MARK: - Lifecycle
 
