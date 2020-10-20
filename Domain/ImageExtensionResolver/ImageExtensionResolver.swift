4902c24125674b81ecdb3a6be278f49e7b87464a
tasuwo <kamuhata.you@gmail.com>
2020-10-21 01:53:55 +0900

    :sparkles: URLからkUTTypeを解決できる

--- Domain/ImageExtensionResolver/ImageExtensionResolver.swift
+++ Domain/ImageExtensionResolver/ImageExtensionResolver.swift
@@ -5,15 +5,46 @@
 import MobileCoreServices
 
 public enum ImageExtensionResolver {
-    private static let table: [String: String] = [
-        "image/bmp": "bmp",
-        "image/gif": "gif",
-        "image/jpeg": "jpeg",
-        "image/png": "png",
-        "image/tiff": "tiff"
+    public enum Extension: String {
+        case bmp
+        case gif
+        case jpeg
+        case png
+        case tiff
+
+        var utType: CFString? {
+            switch self {
+            case .bmp:
+                return kUTTypeBMP
+
+            case .gif:
+                return kUTTypeGIF
+
+            case .jpeg:
+                return kUTTypeJPEG
+
+            case .png:
+                return kUTTypePNG
+
+            case .tiff:
+                return kUTTypeTIFF
+            }
+        }
+    }
+
+    private static let table: [String: Extension] = [
+        "image/bmp": .bmp,
+        "image/gif": .gif,
+        "image/jpeg": .jpeg,
+        "image/png": .png,
+        "image/tiff": .tiff
     ]
 
     public static func resolveFileExtension(forMimeType mimeType: String) -> String? {
-        return self.table[mimeType]
+        return self.table[mimeType]?.rawValue
+    }
+
+    public static func resolveUTType(of url: URL) -> CFString {
+        return Extension(rawValue: url.pathExtension)?.utType ?? kUTTypeJPEG
     }
 }
--- DomainTests/ImageExtensionResolver/ImageExtensionResolverSpec.swift
+++ DomainTests/ImageExtensionResolver/ImageExtensionResolverSpec.swift
@@ -2,6 +2,7 @@
 //  Copyright © 2020 Tasuku Tozawa. All rights reserved.
 //
 
+import MobileCoreServices
 import Nimble
 import Quick
 
@@ -20,6 +21,22 @@ class ImageExtensionResolverSpec: QuickSpec {
                 context("MimeTypeが\(mimeType)") {
                     it("拡張子が\(ext)になる") {
                         expect(ImageExtensionResolver.resolveFileExtension(forMimeType: mimeType)).to(equal(ext))
+                    }
+                }
+            }
+        }
+
+        describe("resolveUTType(of:)") {
+            ([
+                (URL(string: "hoge://fuga/piyo/ooo.bmp")!, kUTTypeBMP),
+                (URL(string: "user://file/name/dyo.gif")!, kUTTypeGIF),
+                (URL(string: "howa://piyo/fuga/ooooo123.png")!, kUTTypePNG),
+                (URL(string: "ooo://pipi/pupu.tiff?hoge=fuga&piyo=poro")!, kUTTypeTIFF),
+                (URL(string: "/Users/tasuwo/hogehoge/fuga.jpeg")!, kUTTypeJPEG),
+            ] as [(URL, CFString)]).forEach { url, ext in
+                context("URLが\(url.absoluteString)") {
+                    it("拡張子が\(ext)になる") {
+                        expect(ImageExtensionResolver.resolveUTType(of: url)).to(equal(ext))
                     }
                 }
             }
