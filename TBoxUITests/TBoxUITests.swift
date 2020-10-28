986da8c806b5b52c4bc48cc983c7b4baaf3e7b1f
tasuwo <kamuhata.you@gmail.com>
2020-10-28 23:47:53 +0900

    :construction: Fastlane Snapshot の準備

---
+++ TBoxUITests/TBoxUITests.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
+//
+
+import XCTest
+
+class TBoxUITests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.tabBars["Tab Bar"].buttons["Home"].tap()
+        snapshot("Home")
+
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).tap()
+        snapshot("Preview")
+
+        app.navigationBars["LikePics.ClipPreviewPageView"].buttons["info"].tap()
+        snapshot("Info")
+    }
+}
