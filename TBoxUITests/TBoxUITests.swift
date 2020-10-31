83963d7af7e9231fd232901daed0809656e6db01
tasuwo <kamuhata.you@gmail.com>
2020-11-01 02:06:20 +0900

    :art: format

--- TBoxUITests/TBoxUITests.swift
+++ TBoxUITests/TBoxUITests.swift
@@ -5,29 +5,29 @@
 import XCTest
 
 /*
-class TBoxUITests: XCTestCase {
-    override func setUpWithError() throws {
-        try super.setUpWithError()
+ class TBoxUITests: XCTestCase {
+     override func setUpWithError() throws {
+         try super.setUpWithError()
 
-        let app = XCUIApplication()
-        setupSnapshot(app)
-        app.launch()
-    }
+         let app = XCUIApplication()
+         setupSnapshot(app)
+         app.launch()
+     }
 
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-    }
+     override func tearDownWithError() throws {
+         try super.tearDownWithError()
+     }
 
-    func testExample() throws {
-        let app = XCUIApplication()
-        app.tabBars["Tab Bar"].buttons["Home"].tap()
-        snapshot("Home")
+     func testExample() throws {
+         let app = XCUIApplication()
+         app.tabBars["Tab Bar"].buttons["Home"].tap()
+         snapshot("Home")
 
-        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).tap()
-        snapshot("Preview")
+         app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).tap()
+         snapshot("Preview")
 
-        app.navigationBars["LikePics.ClipPreviewPageView"].buttons["info"].tap()
-        snapshot("Info")
-    }
-}
- */
+         app.navigationBars["LikePics.ClipPreviewPageView"].buttons["info"].tap()
+         snapshot("Info")
+     }
+ }
+  */
