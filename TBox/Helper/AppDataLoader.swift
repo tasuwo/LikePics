8115550b93fb6be07efc90fb5e722db2536ff931
tasuwo <kamuhata.you@gmail.com>
2020-11-02 20:45:09 +0900

    :hammer: 必要な言語に対応したデータをスクショ用に読み込める

--- TBox/Helper/AppDataLoader.swift
+++ TBox/Helper/AppDataLoader.swift
@@ -4,12 +4,20 @@
 
 import Foundation
 
-// swiftlint:disable force_try
-
 enum AppDataLoader {
     static func loadAppData() {
-        guard let contentsURL = Bundle.main.url(forResource: "AppData", withExtension: nil) else {
-            return
+        let contentsURL: URL
+        switch Locale.autoupdatingCurrent.languageCode {
+        case "ja":
+            guard let url = Bundle.main.url(forResource: "AppData/ja-JP", withExtension: nil) else { return }
+            contentsURL = url
+
+        case "en":
+            guard let url = Bundle.main.url(forResource: "AppData/en-US", withExtension: nil) else { return }
+            contentsURL = url
+
+        default:
+            fatalError("Unexpected languageCode: \(Locale.autoupdatingCurrent.languageCode ?? "nil")")
         }
 
         guard let destinationRoot = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).last?.deletingLastPathComponent() else {
@@ -30,9 +38,9 @@ enum AppDataLoader {
             let path = sourceURL.standardizedFileURL.path.replacingOccurrences(of: contentsURL.standardizedFileURL.path, with: "")
             let destinationURL = destinationRoot.appendingPathComponent(path)
 
-            try! FileManager.default.createDirectory(at: destinationURL.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
+            try? FileManager.default.createDirectory(at: destinationURL.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
             try? FileManager.default.removeItem(at: destinationURL)
-            try! FileManager.default.copyItem(at: sourceURL, to: destinationURL)
+            try? FileManager.default.copyItem(at: sourceURL, to: destinationURL)
         }
     }
 }
