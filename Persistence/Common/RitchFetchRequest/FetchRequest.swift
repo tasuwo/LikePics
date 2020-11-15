281666e35172bf473508987b9baea3417f7b5000
tasuwo <kamuhata.you@gmail.com>
2020-11-15 17:09:22 +0900

    :fire: RichFetchResultsControllerの削除

--- Persistence/Common/RitchFetchRequest/FetchRequest.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
-//
-
-import CoreData
-
-class FetchRequest<ResultType>: NSFetchRequest<NSFetchRequestResult> where ResultType: NSFetchRequestResult {
-    var refreshingRelationships: Set<String> = []
-}
