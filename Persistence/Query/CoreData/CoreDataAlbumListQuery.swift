d33d61fec2743dbcf6210ead66d421b85e433592
tasuwo <kamuhata.you@gmail.com>
2020-11-18 02:21:19 +0900

    :bug: アルバムリストのサムネイルを動的に更新する

--- Persistence/Query/CoreData/CoreDataAlbumListQuery.swift
+++ Persistence/Query/CoreData/CoreDataAlbumListQuery.swift
@@ -8,6 +8,7 @@ import Domain
 import UIKit
 
 class CoreDataAlbumListQuery: NSObject {
+    private var albumIds: Set<Domain.Album.Identity>
     private var subject: CurrentValueSubject<[Domain.Album], Error>
     private let controller: NSFetchedResultsController<Album>
 
@@ -17,6 +18,7 @@ class CoreDataAlbumListQuery: NSObject {
         let currentAlbums = try context.fetch(request)
             .compactMap { $0.map(to: Domain.Album.self) }
 
+        self.albumIds = Set(currentAlbums.map({ $0.id }))
         self.subject = .init(currentAlbums)
         self.controller = NSFetchedResultsController(fetchRequest: request,
                                                      managedObjectContext: context,
@@ -27,6 +29,37 @@ class CoreDataAlbumListQuery: NSObject {
 
         self.controller.delegate = self
         try self.controller.performFetch()
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(contextDidChangeNotification(notification:)),
+                                               name: NSNotification.Name.NSManagedObjectContextObjectsDidChange,
+                                               object: context)
+    }
+
+    // MARK: - Methods
+
+    @objc
+    private func contextDidChangeNotification(notification: NSNotification) {
+        guard let context = notification.object as? NSManagedObjectContext else { return }
+        context.perform { [weak self] in
+            guard let self = self else { return }
+            if let objects = notification.userInfo?[NSRefreshedObjectsKey] as? Set<NSManagedObject> {
+                for clip in objects.compactMap({ $0 as? Clip }) {
+                    if let albumItem = clip.albumItem?
+                        .allObjects
+                        .compactMap({ $0 as? AlbumItem })
+                        .first(where: { albumItem in
+                            guard let albumId = albumItem.album?.id else { return false }
+                            return self.albumIds.contains(albumId)
+                        }),
+                        let album = albumItem.album
+                    {
+                        context.refresh(album, mergeChanges: true)
+                        return
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -41,6 +74,7 @@ extension CoreDataAlbumListQuery: NSFetchedResultsControllerDelegate {
             let albums: [Domain.Album] = (snapshot as NSDiffableDataSourceSnapshot<Int, NSManagedObjectID>).itemIdentifiers
                 .compactMap { controller.managedObjectContext.object(with: $0) as? Album }
                 .compactMap { $0.map(to: Domain.Album.self) }
+            self.albumIds = Set(albums.map { $0.id })
             self.subject.send(albums)
         }
     }
