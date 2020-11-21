92fbec09ea689b6dc0dba44d507452c25741c6a7
tasuwo <kamuhata.you@gmail.com>
2020-11-21 13:37:51 +0900

    :sparkles: iCloud同期のオン/オフを切り替えても差分をマージできる

--- TBox/Sources/Context/DependencyContainer.swift
+++ TBox/Sources/Context/DependencyContainer.swift
@@ -66,12 +66,19 @@ class DependencyContainer {
 
     lazy var persistentContainer: NSPersistentCloudKitContainer = {
         let container = PersistentContainerLoader.load()
+
+        guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("Failed to retrieve a persistent store description.")
+        }
+        description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+
         container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
+
         return container
     }()
 
