7a42969f4a4ccab2bfb1fc3a6337ae1ecf9b029c
tasuwo <kamuhata.you@gmail.com>
2020-12-07 23:50:12 +0900

    :sparkles: ClipCollectionのOperationモデルを追加

---
+++ TBox/Sources/Presenter/ClipCollection/ValueObject/ClipCollection_Operation.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
+//
+
+extension ClipCollection {
+    enum Operation {
+        case none
+        case selecting
+        case reordering
+    }
+}
