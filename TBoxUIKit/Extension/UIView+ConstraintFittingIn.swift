1637879df92433f5bc321e1d91bd0c032e158e36
tasuwo <kamuhata.you@gmail.com>
2020-12-13 10:05:57 +0900

    :sparkles: fitさせる拡張を追加

---
+++ TBoxUIKit/Extension/UIView+ConstraintFittingIn.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2020 Tasuku Tozawa. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    func constraints(fittingIn view: UIView) -> [NSLayoutConstraint] {
+        return [
+            self.topAnchor.constraint(equalTo: view.topAnchor),
+            self.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            self.leftAnchor.constraint(equalTo: view.leftAnchor),
+            self.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ]
+    }
+}
