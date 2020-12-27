653c26d99e6f5b74333ecdc53f52d9925756a93e
tasuwo <kamuhata.you@gmail.com>
2020-12-27 21:13:32 +0900

    :truck: Rename Group: Presenter > ViewModel

--- Domain/Services/iCloud/CloudStackLoader.swift
+++ Domain/Services/iCloud/CloudStackLoader.swift
@@ -2,7 +2,96 @@
 //  Copyright © 2020 Tasuku Tozawa. All rights reserved.
 //
 
-public protocol CloudStack: AnyObject {
-    var isCloudSyncEnabled: Bool { get }
-    func reload(isCloudSyncEnabled: Bool)
+import Combine
+
+public protocol CloudStackLoaderObserver: AnyObject {
+    func didAccountChanged(_ loader: CloudStackLoader)
+    func didDisabledICloudSyncByUnavailableAccount(_ loader: CloudStackLoader)
+}
+
+public class CloudStackLoader {
+    private let userSettingsStorage: UserSettingsStorageProtocol
+    private let cloudAvailabilityStore: CloudAvailabilityStore
+    private let cloudStack: CloudStack
+    private let queue = DispatchQueue(label: "net.tasuwo.TBox.Domain.CloudStackLoader")
+
+    private var cancellableBag = Set<AnyCancellable>()
+
+    public weak var observer: CloudStackLoaderObserver?
+
+    // MARK: - Lifecycle
+
+    public init(userSettingsStorage: UserSettingsStorageProtocol,
+                cloudAvailabilityStore: CloudAvailabilityStore,
+                cloudStack: CloudStack)
+    {
+        self.userSettingsStorage = userSettingsStorage
+        self.cloudAvailabilityStore = cloudAvailabilityStore
+        self.cloudStack = cloudStack
+    }
+
+    // MARK: - Methods
+
+    public func startObserveCloudAvailability() {
+        self.cloudAvailabilityStore.state
+            // 初回起動時の分を除く
+            .dropFirst()
+            .compactMap { $0 }
+            .sink { [weak self] availability in
+                self?.queue.sync { self?.didUpdate(cloudAvailability: availability) }
+            }
+            .store(in: &self.cancellableBag)
+
+        self.userSettingsStorage.enabledICloudSync
+            // 初回起動時の分を除く
+            .dropFirst()
+            .sink { [weak self] isICloudSyncEnabled in
+                self?.queue.sync { self?.didUpdate(isICloudSyncEnabled: isICloudSyncEnabled) }
+            }
+            .store(in: &self.cancellableBag)
+    }
+
+    private func didUpdate(cloudAvailability: CloudAvailability) {
+        let enabledICloudSync = self.userSettingsStorage.readEnabledICloudSync()
+        switch (enabledICloudSync, cloudAvailability) {
+        case (true, .available(.none)):
+            self.reloadCloudStackIfNeeded(isCloudSyncEnabled: true)
+
+        case (true, .available(.accountChanged)):
+            self.reloadCloudStackIfNeeded(isCloudSyncEnabled: true)
+            // NOTE: アカウント変更の場合は、リロードの成否にかかわらず通知する
+            self.observer?.didAccountChanged(self)
+
+        case (true, .unavailable):
+            if self.reloadCloudStackIfNeeded(isCloudSyncEnabled: false) {
+                self.observer?.didDisabledICloudSyncByUnavailableAccount(self)
+            }
+
+        case (false, _):
+            self.reloadCloudStackIfNeeded(isCloudSyncEnabled: false)
+        }
+    }
+
+    private func didUpdate(isICloudSyncEnabled: Bool) {
+        guard let cloudAvailability = self.cloudAvailabilityStore.state.value else { return }
+        switch (isICloudSyncEnabled, cloudAvailability) {
+        case (true, .available):
+            self.reloadCloudStackIfNeeded(isCloudSyncEnabled: true)
+
+        case (true, .unavailable):
+            if self.reloadCloudStackIfNeeded(isCloudSyncEnabled: false) {
+                self.observer?.didDisabledICloudSyncByUnavailableAccount(self)
+            }
+
+        case (false, _):
+            self.reloadCloudStackIfNeeded(isCloudSyncEnabled: false)
+        }
+    }
+
+    @discardableResult
+    private func reloadCloudStackIfNeeded(isCloudSyncEnabled: Bool) -> Bool {
+        guard isCloudSyncEnabled != self.cloudStack.isCloudSyncEnabled else { return false }
+        self.cloudStack.reload(isCloudSyncEnabled: isCloudSyncEnabled)
+        return true
+    }
 }
