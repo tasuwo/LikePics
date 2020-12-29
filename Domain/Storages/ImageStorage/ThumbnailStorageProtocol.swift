0256f62bdbc25842cdf6678cbb2330009a1bd74d
tasuwo <kamuhata.you@gmail.com>
2020-12-30 00:18:50 +0900

    :construction: ThumbnailStorageから圧縮処理を削除する

--- Domain/Storages/ImageStorage/ThumbnailStorageProtocol.swift
+++ Domain/Storages/ImageStorage/ThumbnailStorageProtocol.swift
@@ -12,35 +12,7 @@ public protocol ThumbnailStorageProtocol {
      * - attention: for DEBUG
      */
     func clearCache()
-    func readThumbnailIfExists(for item: ClipItem) -> UIImage?
-    func requestThumbnail(for item: ClipItem, completion: @escaping (UIImage?) -> Void)
-    func deleteThumbnailCacheIfExists(for item: ClipItem)
-}
-
-extension ThumbnailStorageProtocol {
-    public static func calcDownsamplingSize(for item: ClipItem) -> CGSize {
-        let imageSize = item.imageSize
-
-        let screenSize = UIScreen.main.bounds.size
-        let rotatedScreenSize = CGSize(width: UIScreen.main.bounds.size.height,
-                                       height: UIScreen.main.bounds.size.width)
-
-        let scaleToFitScreen = max(self.calcScale(forSize: imageSize.cgSize, toFit: screenSize),
-                                   self.calcScale(forSize: imageSize.cgSize, toFit: rotatedScreenSize))
-        let targetScale = min(1, scaleToFitScreen)
-
-        return imageSize.cgSize.scaled(by: targetScale)
-    }
-
-    public static func calcScale(forSize source: CGSize, toFit destination: CGSize) -> CGFloat {
-        let widthScale = destination.width / source.width
-        let heightScale = destination.height / source.height
-        return min(widthScale, heightScale)
-    }
-}
-
-private extension CGSize {
-    func scaled(by scale: CGFloat) -> Self {
-        return .init(width: self.width * scale, height: self.height * scale)
-    }
+    func save(_ image: CGImage, for item: ClipItem) -> Bool
+    func delete(for item: ClipItem) -> Bool
+    func resolveImageUrl(for item: ClipItem) -> URL?
 }
--- Persistence/Storage/FileSystem/ThumbnailStorage.swift
+++ Persistence/Storage/FileSystem/ThumbnailStorage.swift
@@ -18,21 +18,16 @@ public class ThumbnailStorage {
         let targetUrl: URL
     }
 
-    private let queryService: NewImageQueryServiceProtocol
     private let fileManager: FileManager
     private let baseUrl: URL
     private let logger: TBoxLoggable
 
-    private let queue = DispatchQueue(label: "net.tasuwo.TBox.ThumbnailStorage", qos: .userInitiated, attributes: .concurrent)
-
     // MARK: - Lifecycle
 
-    init(queryService: NewImageQueryServiceProtocol,
-         configuration: Configuration,
+    init(configuration: Configuration,
          fileManager: FileManager = .default,
          logger: TBoxLoggable = RootLogger.shared) throws
     {
-        self.queryService = queryService
         self.baseUrl = configuration.targetUrl
         self.fileManager = fileManager
         self.logger = logger
@@ -40,35 +35,13 @@ public class ThumbnailStorage {
         try self.createDirectoryIfNeeded(at: self.baseUrl)
     }
 
-    public convenience init(queryService: NewImageQueryServiceProtocol, bundle: Bundle) throws {
-        try self.init(queryService: queryService,
-                      configuration: .resolve(for: bundle),
+    public convenience init(bundle: Bundle) throws {
+        try self.init(configuration: .resolve(for: bundle),
                       fileManager: .default,
                       logger: RootLogger.shared)
     }
 
     // MARK: - Methods
-
-    public static func downsampledImage(data: Data, to pointSize: CGSize) -> CGImage? {
-        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
-        guard let imageSource = CGImageSourceCreateWithData(data as CFData, imageSourceOptions) else {
-            return nil
-        }
-
-        let maxDimensionInPixels = max(pointSize.width, pointSize.height)
-        let downsampleOptions = [
-            kCGImageSourceCreateThumbnailFromImageAlways: true,
-            kCGImageSourceShouldCacheImmediately: true,
-            kCGImageSourceCreateThumbnailWithTransform: true,
-            kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
-        ] as CFDictionary
-
-        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
-            return nil
-        }
-
-        return downsampledImage
-    }
 
     private func createDirectoryIfNeeded(at url: URL) throws {
         guard !self.fileManager.fileExists(atPath: url.path) else { return }
@@ -84,7 +57,7 @@ public class ThumbnailStorage {
         return self.fileManager.fileExists(atPath: url.path)
     }
 
-    private func readCache(for item: ClipItem) throws -> UIImage {
+    private func readCache(for item: ClipItem) throws -> Data {
         let fileUrl = self.resolveCacheImageFileUrl(for: item)
 
         guard let data = try? Data(contentsOf: fileUrl) else {
@@ -94,78 +67,48 @@ public class ThumbnailStorage {
             throw ThumbnailStorageError.failedToReadData
         }
 
-        guard let image = UIImage(data: data) else {
-            self.logger.write(ConsoleLog(level: .error, message: """
-            Failed to initialize UIImage by Data. (url=\(fileUrl))
-            """))
-            throw ThumbnailStorageError.failedToConvertToImage
-        }
-
-        return image
+        return data
     }
 }
 
 extension ThumbnailStorage: ThumbnailStorageProtocol {
+    // MARK: - ThumbnailStorageProtocol
+
     public func clearCache() {
         guard self.fileManager.fileExists(atPath: self.baseUrl.path) else { return }
         try? self.fileManager.removeItem(at: self.baseUrl)
         try? self.createDirectoryIfNeeded(at: self.baseUrl)
     }
 
-    public func readThumbnailIfExists(for item: ClipItem) -> UIImage? {
-        guard self.existsCache(for: item) else { return nil }
-        return try? self.readCache(for: item)
-    }
-
-    public func requestThumbnail(for item: ClipItem, completion: @escaping (UIImage?) -> Void) {
-        guard !self.existsCache(for: item) else {
-            guard let image = try? self.readCache(for: item) else {
-                completion(nil)
-                return
-            }
-            completion(image)
-            return
-        }
-
-        self.queue.async {
-            guard let data = try? self.queryService.read(having: item.imageId) else {
-                self.logger.write(ConsoleLog(level: .debug, message: """
-                元画像ファイルの取得に失敗しました。iCloud同期前である可能性があります (name=\(item.imageFileName), clipId=\(item.clipId))
-                """))
-                completion(nil)
-                return
-            }
-
-            let targetSize = Self.calcDownsamplingSize(for: item)
-
-            guard let downsampledImage = Self.downsampledImage(data: data, to: targetSize) else {
-                self.logger.write(ConsoleLog(level: .error, message: """
-                Failed to downsampling image. (name=\(item.imageId).\(item.imageFileName) targetSize=\(targetSize)
-                """))
-                completion(nil)
-                return
-            }
-
-            let fileUrl = self.resolveCacheImageFileUrl(for: item)
-            let utType = ImageExtensionResolver.resolveUTType(of: fileUrl)
-            guard let destination = CGImageDestinationCreateWithURL(fileUrl as CFURL, utType, 1, nil) else {
-                self.logger.write(ConsoleLog(level: .error, message: """
-                Failed to create CGIMageDestination. (fileUrl=\(fileUrl.absoluteString), targetSize=\(targetSize))
-                """))
-                completion(nil)
-                return
-            }
-
-            CGImageDestinationAddImage(destination, downsampledImage, nil)
-            CGImageDestinationFinalize(destination)
-
-            completion(UIImage(cgImage: downsampledImage))
-        }
-    }
-
-    public func deleteThumbnailCacheIfExists(for item: ClipItem) {
-        guard self.existsCache(for: item) else { return }
+    public func save(_ image: CGImage, for item: ClipItem) -> Bool {
         let fileUrl = self.resolveCacheImageFileUrl(for: item)
-        try? self.fileManager.removeItem(at: fileUrl)
+        let utType = ImageExtensionResolver.resolveUTType(of: fileUrl)
+        guard let destination = CGImageDestinationCreateWithURL(fileUrl as CFURL, utType, 1, nil) else {
+            self.logger.write(ConsoleLog(level: .error, message: """
+            Failed to create CGImageDestination. (fileUrl=\(fileUrl.absoluteString))
+            """))
+            return false
+        }
+
+        CGImageDestinationAddImage(destination, image, nil)
+        CGImageDestinationFinalize(destination)
+
+        return true
+    }
+
+    public func delete(for item: ClipItem) -> Bool {
+        guard self.existsCache(for: item) else { return true }
+        let fileUrl = self.resolveCacheImageFileUrl(for: item)
+        do {
+            try self.fileManager.removeItem(at: fileUrl)
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    public func resolveImageUrl(for item: ClipItem) -> URL? {
+        guard self.existsCache(for: item) else { return nil }
+        return self.resolveCacheImageFileUrl(for: item)
     }
 }
