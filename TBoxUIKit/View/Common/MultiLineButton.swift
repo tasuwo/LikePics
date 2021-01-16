a88ce0a371f1d866bf46f4db0ed40c65e7277ca7
tasuwo <kamuhata.you@gmail.com>
2021-01-16 22:06:51 +0900

    :lipstick: 複数行のボタンの高さの計算ロジックを修正する

--- TBoxUIKit/View/Common/MultiLineButton.swift
+++ TBoxUIKit/View/Common/MultiLineButton.swift
@@ -5,6 +5,23 @@
 import UIKit
 
 class MultiLineButton: UIButton {
+    override var bounds: CGRect {
+        didSet {
+            if !oldValue.equalTo(bounds) {
+                invalidateIntrinsicContentSize()
+            }
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        guard let titleLabel = titleLabel else { return .zero }
+        let labelSize = titleLabel.sizeThatFits(CGSize(width: frame.width - (titleEdgeInsets.left + titleEdgeInsets.right),
+                                                       height: .greatestFiniteMagnitude))
+        let desiredButtonSize = CGSize(width: labelSize.width + contentEdgeInsets.left + contentEdgeInsets.right,
+                                       height: labelSize.height + contentEdgeInsets.top + contentEdgeInsets.bottom)
+        return desiredButtonSize
+    }
+
     // MARK: - Lifecycle
 
     required init?(coder aDecoder: NSCoder) {
@@ -13,20 +30,10 @@ class MultiLineButton: UIButton {
         self.setupAppearance()
     }
 
+    // MARK: - Methods
+
     private func setupAppearance() {
         self.titleLabel?.numberOfLines = 0
         self.titleLabel?.lineBreakMode = .byWordWrapping
-    }
-
-    // MARK: - Overrides (UIButton)
-
-    override var intrinsicContentSize: CGSize {
-        return titleLabel?.intrinsicContentSize ?? CGSize.zero
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        titleLabel?.preferredMaxLayoutWidth = titleLabel?.frame.size.width ?? 0
-        super.layoutSubviews()
     }
 }
