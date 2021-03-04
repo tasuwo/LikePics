dde5760ec05f8141fea0b577f2fa39fa54ccf77d
tasuwo <kamuhata.you@gmail.com>
2021-03-05 02:04:13 +0900

    :recycle: Refactor

--- TBox/Sources/Sandbox/App/Component/ClipCollection/NavigationBar/Store/Dependency/HasClipCollectionNavigationBarDelegate.swift
+++ TBox/Sources/Sandbox/App/Component/ClipCollection/NavigationBar/Store/Dependency/HasClipCollectionNavigationBarDelegate.swift
@@ -2,6 +2,8 @@
 //  Copyright © 2021 Tasuku Tozawa. All rights reserved.
 //
 
+// swiftlint:disable class_delegate_protocol
+
 protocol HasClipCollectionNavigationBarDelegate {
     var clipCollectionNavigationBarDelegate: ClipCollectionNavigationBarDelegate? { get }
 }
