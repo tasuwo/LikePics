8a127a7c54da7638ecfb0e5eecdecdd5fa26a5ce
tasuwo <kamuhata.you@gmail.com>
2021-04-30 21:08:44 +0900

    :wrench: ビルド時にswiftlintが実行できていなかった問題を修正する

--- scripts/run_swiftlint.sh
+++ scripts/run_swiftlint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [[ -f "${PODS_ROOT}/SwiftLint/swiftlint" ]]; then
-  "${PODS_ROOT}/SwiftLint/swiftlint"
+if [[ -f "${PROJECT_DIR}/Pods//SwiftLint/swiftlint" ]]; then
+  "${PROJECT_DIR}/Pods/SwiftLint/swiftlint"
 else
   echo "warning: SwiftLint is not installed. Run 'bundle exec pod install --repo-update' to install it."
 fi
