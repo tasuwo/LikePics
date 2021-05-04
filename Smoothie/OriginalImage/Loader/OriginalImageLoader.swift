322e0f9cf2df9694b7aff67703b0228a1155622c
tasuwo <kamuhata.you@gmail.com>
2021-05-04 20:55:30 +0900

    :truck: Smoothieのディレクトリ構成を整理する

--- Smoothie/OriginalImage/Loader/OriginalImageLoader.swift
+++ Smoothie/OriginalImage/Loader/OriginalImageLoader.swift
@@ -2,8 +2,6 @@
 //  Copyright © 2020 Tasuku Tozawa. All rights reserved.
 //
 
-public protocol OriginalImageRequest {}
-
 public protocol OriginalImageLoader {
     func loadData(with request: OriginalImageRequest) -> Data?
 }
