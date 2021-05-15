382ff5743b886442addf83a76bd01e7bcbb00584
tasuwo <kamuhata.you@gmail.com>
2021-05-15 21:23:05 +0900

    :recycle: CloudAvailabilityをCodableに適合させる

--- Domain/ValueObject/Cloud/CloudAvailability.swift
+++ Domain/ValueObject/Cloud/CloudAvailability.swift
@@ -6,9 +6,9 @@ public enum CloudAvailability: Equatable {
     case available(Context)
     case unavailable
 
-    public enum Context: Equatable {
+    public enum Context: String, Equatable, Codable {
         case none
-        case accountChanged
+        case accountChanged = "account_changed"
     }
 
     public var isAvailable: Bool {
@@ -18,6 +18,44 @@ public enum CloudAvailability: Equatable {
 
         case .unavailable:
             return false
+        }
+    }
+}
+
+extension CloudAvailability: Codable {
+    // MARK: - Codable
+
+    enum CodingKeys: CodingKey {
+        case available
+        case unavailable
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let key = container.allKeys.first
+
+        switch key {
+        case .available:
+            let context = try container.decode(Context.self, forKey: .available)
+            self = .available(context)
+
+        case .unavailable:
+            self = .unavailable
+
+        default:
+            throw DecodingError.dataCorrupted(.init(codingPath: container.codingPath, debugDescription: "Unable to decode"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        switch self {
+        case let .available(context):
+            try container.encode(context, forKey: .available)
+
+        case .unavailable:
+            try container.encode(true, forKey: .unavailable)
         }
     }
 }
---
+++ DomainTests/ValueObject/Cloud/CloudAvailabilitySpec.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
+//
+
+import Nimble
+import Quick
+
+@testable import Domain
+
+class CloudAvailabilitySpec: QuickSpec {
+    override func spec() {
+        describe("init(from:)") {
+        }
+
+        describe("encode(to:)") {
+            ([
+                .available(.none),
+                .available(.accountChanged),
+                .unavailable
+            ] as [CloudAvailability]).forEach { availability in
+                it("エンコードできる") {
+                    let encoder = JSONEncoder()
+                    let data = try! encoder.encode(availability)
+
+                    let decoder = JSONDecoder()
+                    let decoded = try! decoder.decode(CloudAvailability.self, from: data)
+
+                    expect(decoded).to(equal(availability))
+                }
+            }
+        }
+    }
+}
