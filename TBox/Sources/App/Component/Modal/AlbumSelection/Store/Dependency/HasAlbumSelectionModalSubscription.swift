e87a77bfbf057545814473ca9dffa0992c02a084
tasuwo <kamuhata.you@gmail.com>
2021-05-21 00:49:49 +0900

    :fire: 不要ファイルの削除

--- TBox/Sources/App/Component/Modal/AlbumSelection/Store/Dependency/HasAlbumSelectionModalSubscription.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
-//
-
-import Domain
-
-protocol HasAlbumSelectionModalSubscription {
-    var albumSelectionCompleted: (Album.Identity?) -> Void { get }
-}
