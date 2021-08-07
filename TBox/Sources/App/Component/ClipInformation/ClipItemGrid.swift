68e45901eb149bc95dfffd03704ca3c2214b98d4
tasuwo <kamuhata.you@gmail.com>
2021-08-07 10:55:21 +0900

    :sparkles: ClipItemをreorderできる

--- TBox/Sources/App/Component/ClipInformation/ClipItemGrid.swift
+++ TBox/Sources/App/Component/ClipInformation/ClipItemGrid.swift
@@ -5,16 +5,49 @@
 import Domain
 import SwiftUI
 
+struct ClipItemGridDropDelegate: DropDelegate {
+    let item: ClipItemGrid.ClipItem
+    @Binding var clipItems: [ClipItemGrid.ClipItem]
+    @Binding var dragging: ClipItemGrid.ClipItem?
+
+    // MARK: - DropDelegate
+
+    func validateDrop(info: DropInfo) -> Bool {
+        true
+    }
+
+    func performDrop(info: DropInfo) -> Bool {
+        dragging = nil
+        return false
+    }
+
+    func dropEntered(info: DropInfo) {
+        guard dragging != item,
+              let dragging = dragging,
+              let from = clipItems.firstIndex(of: dragging),
+              let to = clipItems.firstIndex(of: item),
+              from != to
+        else {
+            return
+        }
+
+        clipItems.move(fromOffsets: IndexSet(integer: from),
+                       toOffset: to > from ? to + 1 : to)
+    }
+
+    func dropUpdated(info: DropInfo) -> DropProposal? {
+        return DropProposal(operation: .move)
+    }
+}
+
 struct ClipItemGrid: View {
-    struct ClipItem: Swift.Identifiable {
+    struct ClipItem: Equatable, Hashable, Swift.Identifiable {
         let id: UUID
         let imageFileName: String
         let imageDataSize: Int
     }
 
     static let spacing: CGFloat = 8.0
-
-    let clipItems: [ClipItem]
 
     var layout: [GridItem] {
         Array(repeating: GridItem(.flexible()), count: horizontalItemCount)
@@ -33,21 +66,39 @@ struct ClipItemGrid: View {
         }
     }
 
+    @State var clipItems: [ClipItem]
+    @State private var dragging: ClipItem?
+
     @Environment(\.horizontalSizeClass) var sizeClass
 
     var body: some View {
         ScrollView(.vertical, showsIndicators: true) {
             LazyVGrid(columns: layout, alignment: .center, spacing: Self.spacing) {
-                ForEach(Array(zip(clipItems.indices, clipItems)), id: \.0) { index, item in
-                    ClipItemCell(image: Self.imageWithColor(color: .red, size: .init(width: 100, height: 100)),
-                                 fileName: item.imageFileName,
-                                 dataSize: item.imageDataSize,
-                                 page: index,
-                                 numberOfPage: clipItems.count)
+                ForEach(Array(zip(clipItems.indices, clipItems)), id: \.1) { index, item in
+                    if dragging == item {
+                        clipItemCell(item, at: index)
+                            .hidden()
+                    } else {
+                        clipItemCell(item, at: index)
+                            .onDrag {
+                                dragging = item
+                                return NSItemProvider(object: item.id.uuidString as NSString)
+                            }
+                            .onDrop(of: [.text], delegate: ClipItemGridDropDelegate(item: item, clipItems: $clipItems, dragging: $dragging))
+                    }
                 }
             }
+            .animation(.default, value: clipItems)
             .padding(.all, Self.spacing)
         }
+    }
+
+    func clipItemCell(_ item: ClipItem, at index: Int) -> ClipItemCell {
+        return ClipItemCell(image: Self.imageWithColor(color: .red, size: .init(width: 100, height: 100)),
+                            fileName: item.imageFileName,
+                            dataSize: item.imageDataSize,
+                            page: index,
+                            numberOfPage: clipItems.count)
     }
 
     static func imageWithColor(color: UIColor, size: CGSize) -> UIImage {
@@ -60,6 +111,8 @@ struct ClipItemGrid: View {
         return image!
     }
 }
+
+// MARK: - Preview
 
 struct ClipItemGrid_Previews: PreviewProvider {
     static var previews: some View {
