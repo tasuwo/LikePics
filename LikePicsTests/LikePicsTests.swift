96a8f40ad4318e651ab0786c439b3aa412d966e0
tasuwo <kamuhata.you@gmail.com>
2021-09-11 08:49:33 +0900

    :truck: Rename: TBox > LikePics

--- LikePicsTests/LikePicsTests.swift
+++ LikePicsTests/LikePicsTests.swift
@@ -9,7 +9,7 @@
 @testable import LikePics
 import XCTest
 
-class TBoxTests: XCTestCase {
+class LikePicsTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
