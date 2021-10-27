83a21c7352ff3210e8596745fff92f427d08f297
tasuwo <kamuhata.you@gmail.com>
2021-10-27 22:31:19 +0900

    :wave: xcodegenの利用をなくす

--- scripts/run_swiftlint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ -f "${PROJECT_DIR}/Pods//SwiftLint/swiftlint" ]]; then
-  "${PROJECT_DIR}/Pods/SwiftLint/swiftlint"
-else
-  echo "warning: SwiftLint is not installed. Run 'bundle exec pod install --repo-update' to install it."
-fi
