5419add9520f5cd6b2d1741a0d56dce5d9725ff7
tasuwo <kamuhata.you@gmail.com>
2022-03-18 00:14:01 +0900

    :sparkles: ClipsIntegrityValidatorを実装

---
+++ LikePicsKit/Sources/AppFeature/App/Root/ClipsIntegrityValidator/ClipsIntegrityValidator.swift
@@ -0,0 +1,28 @@
+//
+//  Copyright ©︎ 2022 Tasuku Tozawa. All rights reserved.
+//
+
+import CompositeKit
+import LikePicsCore
+
+class ClipsIntegrityValidator {
+    typealias Store = CompositeKit.Store<ClipsIntegrityValidatorState, ClipsIntegrityValidatorAction, ClipsIntegrityValidatorDependency>
+    
+    // MARK: - Properties
+    
+    // MARK: Store
+    private var store: Store
+    
+    // MARK: - Initializers
+    
+    init(dependency: ClipsIntegrityValidatorDependency) {
+        self.store = .init(initialState: ClipsIntegrityValidatorState(),
+                           dependency: dependency,
+                           reducer: ClipsIntegrityValidatorReducer())
+        
+        DarwinNotificationCenter.default.addObserver(self, for: .shareExtensionDidCompleteRequest) { [weak self] _ in
+            self?.store.execute(.shareExtensionDidCompleteRequest)
+        }
+        self.store.execute(.didLaunchApp)
+    }
+}
