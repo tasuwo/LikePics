cfb6a121ea2776d449543007035c53eccfdf13dd
tasuwo <kamuhata.you@gmail.com>
2024-01-01 02:21:30 +0900

    :recycle: SwiftUI版でIdentifiableに適合させる

---
+++ LikePicsKit/Sources/AppDesktopFeature/Identifiable/Domain+Identifiable.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright ©︎ 2024 Tasuku Tozawa. All rights reserved.
+//
+
+import Domain
+
+extension Album: Swift.Identifiable {}
+extension ListingAlbumTitle: Swift.Identifiable {}
+extension Clip: Swift.Identifiable {}
+extension ClipItem: Swift.Identifiable {}
+extension Tag: Swift.Identifiable {}
