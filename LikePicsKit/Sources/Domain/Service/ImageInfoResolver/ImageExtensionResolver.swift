e1500c70bb6f59867ee4f8b775968059bad27ce9
tasuwo <kamuhata.you@gmail.com>
2024-01-02 10:51:07 +0900

    :sparkles: データから拡張子を判別できる

--- LikePicsKit/Sources/Domain/Service/ImageInfoResolver/ImageExtensionResolver.swift
+++ LikePicsKit/Sources/Domain/Service/ImageInfoResolver/ImageExtensionResolver.swift
@@ -48,4 +48,30 @@ public enum ImageExtensionResolver {
     public static func resolveUTType(of url: URL) -> UTType {
         return Extension(rawValue: url.pathExtension)?.utType ?? .jpeg
     }
+
+    public static func resolveFileExtension(for data: Data) -> String? {
+        // https://en.wikipedia.org/wiki/JPEG
+        if data.starts(with: [0xff, 0xd8, 0xff]) { return "jpeg" }
+        // https://en.wikipedia.org/wiki/Portable_Network_Graphics
+        if data.starts(with: [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]) { return "png" }
+        // https://en.wikipedia.org/wiki/GIF
+        if data.starts(with: [0x47, 0x49, 0x46]) { return "gif" }
+        // https://en.wikipedia.org/wiki/List_of_file_signatures
+        if data.starts(with: [0x52, 0x49, 0x46, 0x46, nil, nil, nil, nil, 0x57, 0x45, 0x42, 0x50]) { return "webp" }
+        // https://en.wikipedia.org/wiki/List_of_file_signatures
+        if data.starts(with: [0x4d, 0x4d, 0x00, 0x2a]) { return "tiff" }
+        // https://en.wikipedia.org/wiki/List_of_file_signatures
+        if data.starts(with: [0x42, 0x4d]) { return "bmp" }
+        return nil
+    }
+}
+
+private extension Data {
+    func starts(with numbers: [UInt8?]) -> Bool {
+        guard self.count >= numbers.count else { return false }
+        return zip(numbers.indices, numbers).allSatisfy { index, number in
+            guard let number = number else { return true }
+            return self[index] == number
+        }
+    }
 }
